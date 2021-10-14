f74d6367207d0292c57df8b6bb3a3343c8461128
software3000 <desarrollo@software3000.net>
2021-10-14 16:59:36 -0500

    primer commit

---
+++ FirmarPfd/Clases/Firmante.cs
@@ -0,0 +1,40 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.security;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirmarPfd.Clases
+{
+    public class Firmante
+    {
+        private readonly Certificado certificado;
+
+        public Firmante(Certificado certificado)
+        {
+            this.certificado = certificado;
+        }
+
+        public void Firmar(string rutaDocumentoSinFirma, string rutaDocumentoFirmado)
+        {
+            using (var reader = new PdfReader(rutaDocumentoSinFirma))
+            using (var writer = new FileStream(rutaDocumentoFirmado, FileMode.Create, FileAccess.Write))
+            using (var stamper = PdfStamper.CreateSignature(reader, writer, '\0', null, true))
+            {
+                var signature = stamper.SignatureAppearance;
+                signature.CertificationLevel = PdfSignatureAppearance.CERTIFIED_NO_CHANGES_ALLOWED;
+                signature.Reason = "Firma del Boletas GDT";
+                signature.ReasonCaption = "Tipo de firma: ";
+                signature.Location = "Tacna - Peru";
+                var signatureKey = new PrivateKeySignature(certificado.Key, DigestAlgorithms.SHA256);
+                var signatureChain = certificado.Chain;
+                var standard = CryptoStandard.CADES;
+
+                MakeSignature.SignDetached(signature, signatureKey, signatureChain, null, null, null, 0, standard);
+            }
+        }
+    }
+}
---
+++ FirmarPfd/Program.cs
@@ -0,0 +1,29 @@
+﻿using FirmarPfd.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirmarPfd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var certificado = new Certificado(@"F:\demos\VidalTI.pfx", "VidalTI*");
+            var firmante = new Firmante(certificado);
+            firmante.Firmar(@"F:\demos\documento.pdf", @"F:\demos\documento-firmado.pdf");
+            //var certificado = new Certificado(@"F:\demos\VidalTI.pfx", "VidalTI*");
+            //var notario = new Notario(certificado);
+            //var documentoValido = notario.CertificarDocumento(@"F:\demos\documento-firmado.pdf");
+
+            //if (documentoValido)
+            //    Console.WriteLine("Documento firmado por el sistema y no ha sufrido modificaciones");
+            //else
+            //    Console.WriteLine("El documento no se pudo validar");
+
+            Console.ReadLine();
+        }
+    }
+}
